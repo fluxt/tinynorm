17c6346d1bf8b57c5f89ba8279371727ffca6230
Hwanseo Choi <electricvoxel@gmail.com>
2024-08-06 17:04:27 -0700

    Update norm.py

--- norm.py
+++ norm.py
@@ -1,16 +1,35 @@
 import torch
-from einops import reduce
+import einops
 
-def batchnorm(x, gamma, beta, eps=1e-5):
-    mean = reduce(x, 'n c h w -> c', 'mean')
-    variance = reduce(x, 'n c h w -> c', 'var')
-    mean = rearrange(mean, 'c -> 1 c 1 1')
-    variance = rearrange(variance, 'c -> 1 c 1 1')
-    x_normalized = (x - mean) / torch.sqrt(variance + eps)
-    out = gamma * x_normalized + beta
-    return out
 
-x_4d = torch.randn(8, 16, 32, 32)
-gamma = torch.ones(1, 16, 1, 1)
-beta = torch.zeros(1, 16, 1, 1)
-output_4d = batchnorm(x_4d, gamma, beta)
+x = torch.randn(8, 16, 32, 32)
+scale = torch.ones(1, 16, 1, 1)
+bias = torch.zeros(1, 16, 1, 1)
+running_mean = torch.zeros(1, 16, 1, 1)
+running_var = torch.ones(1, 16, 1, 1)
+
+
+def batchnorm(x, scale, bias, running_mean, running_var, momentum=0.1, eps=1e-5):
+    mean = einops.reduce(x, 'n c h w -> 1 c 1 1', 'mean')
+    var = einops.reduce(x, 'n c h w -> 1 c 1 1', 'var')
+    y = (x - mean) / torch.sqrt(var + eps)
+    y = scale * y + bias
+
+    running_mean = (1 - momentum) * running_mean + momentum * mean
+    running_var = (1 - momentum) * running_var + momentum * var
+
+    return y, running_mean, running_var
+
+y, out_running_mean, out_running_var = batchnorm(x, scale, bias, running_mean, running_var)
+
+
+def batchnorm_inference(x, scale, bias, running_mean, running_var, eps=1e-5):
+    y = (x - running_mean) / torch.sqrt(running_var + eps)
+    y = scale * y + bias
+    return y
+
+y_inference = batchnorm_inference(x, scale, bias, running_mean, running_var)
+
+
+def batchnorm_backward():
+    pass
